14f767677078f101f3e7a2ca23a846a9c99b0c46
Theworld7 <Theworld7@users.noreply.github.com>
2026-02-22 07:05:55 +0000

    deploy: 0afc80ccbe53753e8da82536c5eef987f4a887f6

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
-// Chrome Extension Background Service Worker
+// Firefox/Chrome Extension Background Script
+
+// 兼容处理：火狐使用 browser 命名空间，Chrome 使用 chrome
+const chrome = typeof browser !== 'undefined' ? browser : chrome
 
 // 监听扩展安装事件
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('寻影浏览器主页扩展已安装');
-});
+  console.log('寻影浏览器主页扩展已安装')
+})
 
 // 可以在此处添加扩展相关功能
